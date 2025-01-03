ec65c7ba5c6ef1cdc232077c374a5cfd7377f84a
J C Lisboa <lookjota@gmail.com>
2025-01-03 17:17:32 -0300

    Caso de uso de autenticacao

---
+++ src/use-cases/authenticate.ts
@@ -0,0 +1,43 @@
+import { UsersRepository } from "@/repositories/users-repository";
+import { InvalidCredentialsError } from "./error/invalid-credentials-error";
+import { compare } from "bcryptjs";
+import { User } from '@prisma/client'
+
+interface AuthenticateUseCaseRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateUseCaseResponse {
+  user: User
+}
+
+export class AuthenticateUseCase {
+  constructor( private usersRepository: UsersRepository) {}
+
+  async execute({ 
+    email, 
+    password 
+  }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+    //auth
+    // buscar o usuario no banco pelo e-mail
+    // comparar se a senha salva no banco bate com a senha do parametro
+    const user = await this.usersRepository.findByEmail(email)
+    
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+    // Boolean => true
+    
+    const doesPasswordMatches = await compare(password, user.password_hash)
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError()
+    }
+      
+    return {
+      user,
+    }
+
+  }
+}
