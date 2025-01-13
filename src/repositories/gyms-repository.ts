4f78b630fc31cf139db65b6a14d84e9b1e274c7b
J C Lisboa <lookjota@gmail.com>
2025-01-13 17:55:19 -0300

    repositorios de academias

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -1,5 +1,10 @@
 import { Gym, Prisma } from '@prisma/client'
 
+export interface FindManyNearbyParams {
+  latitude: number
+  longitude: number
+}
+
 export interface GymRepository {
   findById(id: string): Promise<Gym | null>
   create(data: Prisma.GymCreateInput): Promise<Gym>
