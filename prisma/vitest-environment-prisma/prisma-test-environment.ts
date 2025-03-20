0f9c0ed13f826968629ea9ae165e206730e8ace8
lookjota <lookjota@gmail.com>
2025-03-20 18:41:39 +0000

    Test Environment do Prisma

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,15 +1,43 @@
 
+import { randomUUID } from "node:crypto";
 import { Environment } from "vitest";
+import 'dotenv/config' 
+import { execSync } from "node:child_process";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+// postgresql://docker:docker@localhost:5432/mydb?schema=public
+
+function generateDatabaseURL(schema: string) {
+  if(!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default<Environment>{
   name: 'prisma',
   transformMode: 'web',
-  async setup(global, options) {
-    console.log('setup');
+  async setup() {
+    const schema = randomUUID()
+    const databaseURL = generateDatabaseURL(schema)
+
+    process.env.DATABASE_URL = databaseURL 
+
+    execSync('npx prisma migrate deploy')
 
     return {
-      teardown() { 
-        console.log('teardown');  ``
+      async teardown() { 
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+        await prisma.$disconnect()
       },
     }
   },
