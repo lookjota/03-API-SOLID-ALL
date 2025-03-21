fe138b9ca4b37edf7c48b1a777ade04527875165
J C Lisboa <lookjota@gmail.com>
2025-03-21 11:05:14 -0300

    Outros controllers da academia

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,6 +1,14 @@
 import { FastifyInstance } from 'fastify'
 import { verifyJWT } from '../../middlewares/verify-jwt'
+import { nearby } from './nearby'
+import { search } from './search'
+import { create } from './create'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
+
+  app.get('/gyms/search', search)
+  app.get('/gyms/nearby', nearby)
+
+  app.post('/gyms', create)
 }
